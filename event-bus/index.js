ed2cd80d81c13f5e1eef35475c2e46cc8990db96
Noble Obioma <nobioma1@gmail.com>
2020-05-06 21:56:28 +0100

    Setup an event bus server that receives events and broadcasts it to other services

---
+++ event-bus/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/events', (req, res) => {
+  const events = req.body;
+
+  axios.post('http://localhost:4000', events);
+  axios.post('http://localhost:4001', events);
+  axios.post('http://localhost:4002', events);
+
+  res.status(200).send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Listening on PORT 4005');
+});
