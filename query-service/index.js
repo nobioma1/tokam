c327d3caa5a251f164cf10b1195da7c2040136cf
Noble Obioma <nobioma1@gmail.com>
2020-05-07 00:44:06 +0100

    Create endpoint to handle events from the event-bus - Handle PostCreated event - Handle CommentCreated event

--- query-service/index.js
+++ query-service/index.js
@@ -1,3 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const posts = {};
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title, timestamp } = data;
+    posts[id] = {
+      id,
+      title,
+      comments: [],
+      timestamp,
+    };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, comment, postId, timestamp } = data;
+    const post = posts[postId];
+    post.comments.push({
+      id,
+      comment,
+      postId,
+      timestamp,
+    });
+  }
+  return res.status(200).end();
+});
 app.listen(4002, () => {
   console.log('Listening on PORT 4002');
 });
