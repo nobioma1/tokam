cb0c110b6ecc0c3fb3106fc46411886dad6ec833
Noble Obioma <nobioma1@gmail.com>
2020-05-07 10:29:15 +0100

    Handle event CommentCreated in moderation service - Moderate comments - Emit event CommentModerated to the event-bus

--- moderation-service/index.js
+++ moderation-service/index.js
@@ -5,8 +5,24 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/events', (req, res) => {
+function isModerate(string) {
+  return string.toLowerCase().includes('fuck');
+}
+
+app.post('/events', async (req, res) => {
   const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = isModerate() ? 'approved' : 'rejected';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        ...data,
+        status,
+      },
+    });
+  }
 
   return res.status(200).end();
 });
